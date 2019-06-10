a7bc50888b874f2f49f32caac712c88ddf960295
Mirosław Buszek <zoowett@gmail.com>
2019-06-10 23:01:54 +0200

    Add index.html

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const chalk = require('chalk')
+const debug = require('debug')('app')
+const morgan = require('morgan')
+const path = require('path')
 
 const app = express();
 
+app.use(morgan('tiny'))
 
-app.get('/', function(req, res){
-    res.send('Hello from my app')
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
 })
 
-app.listen(3000, function(){
-    console.log('listening on port ' + chalk.green('3000'));
+app.listen(3000, () => {
+    debug(`listening on port ${chalk.green('3000')}`);
     
 })
