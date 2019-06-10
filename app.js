429919a0d317c37bca9bae819adfedc32925f845
Mirosław Buszek <zoowett@gmail.com>
2019-06-10 12:51:48 +0200

    Add chalk feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const chalk = require('chalk')
 
 const app = express();
 
@@ -8,6 +9,6 @@ app.get('/', function(req, res){
 })
 
 app.listen(3000, function(){
-    console.log('listening on port 3000');
+    console.log('listening on port ' + chalk.green('3000'));
     
 })
