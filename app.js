2a191f9c7e520f7255fcaaa6c3984c3d97080cc4
Mirosław Buszek <zoowett@gmail.com>
2019-06-10 12:39:46 +0200

    Create server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', function(req, res){
+    res.send('Hello from my app')
+})
+
+app.listen(3000, function(){
+    console.log('listening on port 3000');
+    
+})
